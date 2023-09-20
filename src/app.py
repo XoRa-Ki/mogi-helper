b4e841580afb7f469955f24ebf606096aac8f356
MORTAIGNE Massimiliano <massimiliano.mortaigne@telecomnancy.eu>
2023-09-20 09:19:45 +0200

    Bayesic structure and Pikmin 3 reference

---
+++ src/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home() :
+    return "Test"
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('PNF404.html'), 404
+
+@app.route('/manager', methods=['GET', 'POST'])
+def manager() :
+    if request.method == 'POST' :
+        #Redirects to the new generated link that keep track of the mogi
+        #The display link will be found on the new page
+        return "poster"
+    else :
+        #Display the form that ask for the informations needed to generate the link
+        return "getter"
+
+@app.route('/tracks')
+def tracks() :
+    return "Tracks"
+
+@app.route('/bill')
+def bill() :
+    return "Bill Extends !!!"
